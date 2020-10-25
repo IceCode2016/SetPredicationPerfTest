58031bcdbc2de37fa5317aa09361e27f2c2c593b
IceCode2016 <IMYT1989@gmail.com>
2020-10-26 02:13:54 +0900

    Add files via upload

---
+++ x64/Debug/ClearPredicate.hlsl
@@ -0,0 +1,19 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+RWStructuredBuffer<uint> _Predicate : register(u0);    // UAV
+
+[numthreads(1, 1, 1)]
+void CSMain(uint3 Gid : SV_GroupID, uint3 DTid : SV_DispatchThreadID, uint3 GTid : SV_GroupThreadID, uint GI : SV_GroupIndex)
+{
+	_Predicate[0] = 1;
+	_Predicate[1] = 1;
+}
